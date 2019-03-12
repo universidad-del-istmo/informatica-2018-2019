b163aa2786b1050609afdafbfbd934d227057762
Ernesto Rodriguez <erodriguez@unis.edu.gt>
2019-03-12 07:41:50 -0600

    Lista encadenada

--- clases/semana 8/ejemplos/main.cc
+++ clases/semana 8/ejemplos/main.cc
@@ -1,19 +1,30 @@
 #include <stdio.h>
 
+#include "Container.hh"
+#include "Lista.hh"
 #include "Vector.hh"
 
-int main(){
-    Vector v(5);
+double promedio(Container& valores){
 
-    v[3] = 42;
-    double x = v[3];
-
-    if(v[3] < 50){
-        Vector v2(6);
-        v2[3] = 3;
+    double resultado = 0;
+    for(int i = 0; i < valores.size(); i++){
+        resultado += valores[i];
     }
 
+    return resultado / valores.size();
+}
 
-    printf("El valor es %f\n", x);
-    printf("El tamano es %i\n", v.size());
+int main(){
+    Vector v(3);
+    v[0] = 1;
+    v[1] = 5;
+    v[2] = -8;
+
+    printf("El resultado es %f.\n", promedio(v));
+
+    Lista lista(2);
+    lista.valor() = 5;
+    lista.next()->valor() = 6;
+    printf("El valor 1 es %f\n", lista.valor());
+    printf("El valor 2 es %f\n", lista.next()->valor());
 }
