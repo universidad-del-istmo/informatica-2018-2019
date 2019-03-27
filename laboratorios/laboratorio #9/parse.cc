ac5c64f8b9e74eb546fdbccc1853663b9c9cf9c2
Ernesto Rodriguez <ernesto.rodriguez@quantifio.no>
2019-03-27 17:14:19 -0600

    Laboratorio 9

---
+++ laboratorios/laboratorio #9/parse.cc
@@ -0,0 +1,14 @@
+int main(){
+    std::string entrada("1+2*3");
+    Expression* expr;
+
+    // Si se llama a la funcion ''parse'' de la siguiente manera
+    parse(entrada, expr);
+
+    // Es equivalente a construir una expressoin de la siguiente manera
+    Expression* expr2 = new Suma(new Valor(1), new Multiplicacion(2, 3));
+
+    // Por lo tanto la siguiente linea impirme:
+    // "La diferencia es 0.0"
+    printf("La diferencia es %d", expr2.evaluar() - expr.evaluar());
+}
