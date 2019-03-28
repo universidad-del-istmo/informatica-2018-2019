843c0c309d666f5aa8f004cd1ebfc0913682b1ce
Ernesto Rodriguez <ernesto.rodriguez@quantifio.no>
2019-03-28 10:00:44 -0600

    Correciones lab 9

--- laboratorios/laboratorio #9/parse.cc
+++ laboratorios/laboratorio #9/parse.cc
@@ -6,9 +6,9 @@ int main(){
     parse(entrada, expr);
 
     // Es equivalente a construir una expressoin de la siguiente manera
-    Expression* expr2 = new Suma(new Valor(1), new Multiplicacion(2, 3));
+    Expression* expr2 = new Suma(new Valor(1), new Multiplicacion(new Valor(2), new Valor(3)));
 
     // Por lo tanto la siguiente linea impirme:
     // "La diferencia es 0.0"
-    printf("La diferencia es %d", expr2.evaluar() - expr.evaluar());
+    printf("La diferencia es %d", expr2->evaluar() - expr->evaluar());
 }
